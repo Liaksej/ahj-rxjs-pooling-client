de1a4fb33b6b8af248b9d7d0ae6a51237cf6ae31
Liaksej <tarusov.liaksej@gmail.com>
2023-09-02 00:14:19 +0200

    Enhance error handling in messaging request
    Introduced error handling for server requests in the app's messaging feature, specifically for http status code 404 and 500. Imported additional dependencies from rxjs and updated the fetchUnreadMessages$ subscribing method to handle edge-case errors. This increases the robustness of the messaging feature in scenarios where the server is unreachable or responding with an error.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,4 +1,12 @@
-import { interval, switchMap } from "rxjs";
+import {
+  catchError,
+  interval,
+  EMPTY,
+  map,
+  switchMap,
+  throwError,
+  from,
+} from "rxjs";
 import { ajax } from "rxjs/ajax";
 
 import { Message, MessageInterface } from "./Message";
@@ -6,36 +14,46 @@ import { Message, MessageInterface } from "./Message";
 const vault: Message[] = [];
 const list: HTMLUListElement | null = document.querySelector("ul");
 
+interface ServerResponse {
+  status: "ok";
+  timestamp: number;
+  messages: MessageInterface[];
+}
+
 function app() {
   const fetchUnreadMessages$ = interval(3000).pipe(
     switchMap(() =>
-      ajax(
-        "https://ahj-rxjs-pooling-server-liaksej.vercel.app/messages/unread",
-      ),
+      ajax
+        .getJSON<ServerResponse>(
+          "https://ahj-rxjs-pooling-server-liaksej.vercel.app/messages/unread",
+        )
+        .pipe(
+          map((response) => {
+            return response.messages;
+          }),
+          catchError((err) => {
+            if (err.status === 404 || err.status === 500) {
+              console.log("Error", err);
+              return EMPTY;
+            } else {
+              return throwError(() => new Error(err));
+            }
+          }),
+          switchMap((messages) => {
+            return from(messages);
+          }),
+        ),
     ),
   );
 
   fetchUnreadMessages$.subscribe((data) => {
-    const requestData = data.response as {
-      status: "ok";
-      timestamp: number;
-      messages: MessageInterface[];
-    };
-    const messages: MessageInterface[] = requestData.messages;
-    messages.forEach((message) => {
-      const newMessage = new Message(
-        message.id,
-        message.from,
-        message.subject,
-        message.body,
-        message.received,
-      );
+    const { id, from, subject, body, received }: MessageInterface = data;
+    const newMessage = new Message(id, from, subject, body, received);
 
-      vault.push(newMessage);
-      if (list) {
-        newMessage.postMessage(list);
-      }
-    });
+    vault.push(newMessage);
+    if (list) {
+      newMessage.postMessage(list);
+    }
   });
 }
 
