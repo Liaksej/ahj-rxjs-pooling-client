e95ff524cb3efcac4ccd7a822bca05f3ef243f5a
Liaksej <tarusov.liaksej@gmail.com>
2023-09-08 08:40:16 +0200

    Update webpack.prod.js minimizer plugins
    The minimizer in webpack.prod.js was updated to optimize the production build. Rather than using both CssMinimizerPlugin and TerserPlugin, only CssMinimizerPlugin is used now. With `'...'` introduced the purpose is to efficiently spread the existing minimizers in an array while keeping the bundle size small. This change enhances code readability and maintainability while still ensuring efficient minification.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,13 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [new CleanWebpackPlugin()],
   optimization: {
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    minimizer: [`...`, new CssMinimizerPlugin()],
     usedExports: true,
   },
 });
